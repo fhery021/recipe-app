f16516f968f2506adee9ec6e82f4930fafb6f4b4
Ferenc Bondar <fhery021@gmail.com>
2019-07-24 23:50:33 +0300

    recipe-list

--- recipe-app-angular/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app-angular/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Avocado toast', 'make a toast and put avocado on it',
+    'https://wholefully.b-cdn.net/wp-content/uploads/2016/06/avocado-toast-10.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
